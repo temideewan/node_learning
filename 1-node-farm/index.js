6cc017be2f5102fd4bea81be13e12ca1615f37eb
Omoyajowo Temidayo <temideewan@gmail.com>
2023-11-09 11:33:47 +0100

    feat: complet node farm

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,7 +2,7 @@ const fs = require("node:fs");
 const http = require("http");
 const url = require("url");
 
-// 
+//
 /////////////// FILES ////////////////
 
 // blocking synchronous pattern
@@ -36,24 +36,94 @@ const url = require("url");
 /////////////// FILES ////////////////
 
 //////////////////SERVER/////////////////////////////////
+/**
+ *  "id": 0,
+    "productName": "Fresh Avocados",
+    "image": "🥑",
+    "from": "Spain",
+    "nutrients": "Vitamin B, Vitamin K",
+    "quantity": "4 🥑",
+    "price": "6.50",
+    "organic": true,
+    "description": "A ripe avocado yields to gentl
+ */
+/**
+ * @typedef Product
+ * @property {string} productName
+ * @property {string} image
+ * @property {number} id
+ * @property {string} from
+ * @property {boolean} organic
+ * @property {string} description
+ * @property {string} price
+ * @property {string} quantity
+ * @property {string} nutrients
+ *
+ * @param {string} temp
+ * @param {Product} product
+ */
+function replaceTemplate(temp, product) {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+}
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template_overview.html`,
+  "utf-8"
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template_product.html`,
+  "utf-8"
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template_card.html`,
+  "utf-8"
+);
+const dataObj = JSON.parse(data);
+const server = http.createServer((req, res) => {
+  const {query,pathname} = url.parse(req.url, true);
 
-const server =  http.createServer((req, res) => {
-  console.log(req.url);
-  const pathName = req.url;
-  if(pathName === "/" || pathName === '/overview'){
-    res.end("This is the OVERVIEW")
-  }else if (pathName === '/product'){
-    res.end("This is the PRODUCT");
-  }else {
+
+  // Overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHtml = dataObj
+      .map((data) => replaceTemplate(templateCard, data))
+      .join("");
+    const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+    // product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(templateProduct, product);
+    res.end(output);
+
+    // api
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+
+    // not found
+  } else {
     res.writeHead(404, {
-    'Content-Type': 'text/html',
-    'my-own-header': "hello-world"
+      "Content-Type": "text/html",
+      "my-own-header": "hello-world",
     });
-    res.end("<h1>Page not found</h1>")
+    res.end("<h1>Page not found</h1>");
   }
   // res.end("Hello from the server");
-})
+});
 
-server.listen(8000, '127.0.0.1', () => {
+server.listen(8000, "127.0.0.1", () => {
   console.log("Listening on port 8000");
 });
