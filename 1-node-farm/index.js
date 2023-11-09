1fc5fdce96de2a5322c3832e74afeb66e2541e6b
Omoyajowo Temidayo <temideewan@gmail.com>
2023-11-09 12:04:12 +0100

    feat: extract helper function into separate module

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,7 @@
 const fs = require("node:fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require('./module/replaceTemplate')
 
 //
 /////////////// FILES ////////////////
@@ -62,19 +63,6 @@ const url = require("url");
  * @param {string} temp
  * @param {Product} product
  */
-function replaceTemplate(temp, product) {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-}
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const templateOverview = fs.readFileSync(
   `${__dirname}/templates/template_overview.html`,
