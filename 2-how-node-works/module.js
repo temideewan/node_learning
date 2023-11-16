03abe82a078eae4adb3f81866925ad034d330594
Omoyajowo Temidayo <temideewan@gmail.com>
2023-11-16 17:08:26 +0100

    feat: set up for natours project

---
+++ 2-how-node-works/module.js
@@ -0,0 +1,22 @@
+// console.log(arguments);
+// console.log(require('module').wrapper);
+
+// module.exports
+const C = require("./test-module-1");
+
+const calc1 = new C();
+console.log(calc1.add(4, 3));
+
+// exports object
+
+const calc2 = require("./test-module-2");
+const { add, multiply, divide } = require("./test-module-2");
+
+console.log(add(4, 3));
+
+
+// caching
+
+require("./test-module-3")();
+require("./test-module-3")();
+require("./test-module-3")();
