7452100b9daf39ec15215d742493bb857845487f
Omoyajowo Temidayo <temideewan@gmail.com>
2023-11-19 20:02:23 +0100

    feat: intro to CRUD and routing operations with express

---
+++ 4-natours/app.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+// to declare params make it in the pattern "/:{param  name}"
+// to make a parameter optional add in a question mark after it's name "/:{param name}?"
+
+const getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((tour) => tour.id === id);
+  // if(id > tours.length) {
+  if (!tour) {
+    return res
+      .status(404)
+      .json({ status: 'failed', data: { message: 'tour not found' } });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+const updateTour = (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res
+      .status(404)
+      .json({ status: 'failed', data: { message: 'tour not found' } });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'Updated the tour successfully here',
+    },
+  });
+};
+const deleteTour = (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res
+      .status(404)
+      .json({ status: 'failed', data: { message: 'tour not found' } });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}......`);
+});
+
+// 201 stands for something new is created
+// 204 stands for no content
