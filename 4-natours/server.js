99d7b1f43695ec2aa154d35679e189f3c02284e0
Omoyajowo Temidayo <temideewan@gmail.com>
2023-12-01 11:14:28 +0100

    feat: introduce mongoose for interaction with mongoDB

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,51 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB CONNECTION SUCCESSFUL');
+  });
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', toursSchema);
+const testTour = new Tour({
+  name: 'The park camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(`ERROR 🤯 ${err}`);
+  });
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
