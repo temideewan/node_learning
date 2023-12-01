546933b8b4bedde30d250d2d8c63c493812227b4
Omoyajowo Temidayo <temideewan@gmail.com>
2023-12-01 14:16:48 +0100

    feat: first steps to CRUD with mongoose

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,39 +1,13 @@
-const fs = require('fs');
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const Tour = require('../models/tourModel');
 
-// param middleware
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is : ${val}`);
-  const id = req.params.id * 1;
-  const tour = tours.find((tour) => tour.id === id);
-  if (id > tours.length || !tour) {
-    return res
-      .status(404)
-      .json({ status: 'failed', message: 'tour not found' });
-  }
-
-  next();
-};
-exports.checkBody = (req, res, next) => {
-  console.log(req.body);
-  const { name, price } = req.body;
-  if (!name || !price) {
-    return res
-      .status(400)
-      .json({ status: 'failed', message: 'Missing name or price' });
-  }
-  next();
-};
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
@@ -41,41 +15,35 @@ exports.getAllTours = (req, res) => {
 // to make a parameter optional add in a question mark after it's name "/:{param name}?"
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((tour) => tour.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+  // const id = req.params.id * 1;
+  // const tour = tours.find((t) => t.id === id);
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed',
+      message: err,
+    });
+  }
+  // const newTour = new Tour({})
+  // newTour.save();
 };
 exports.updateTour = (req, res) => {
-  const id = req.params.id * 1;
-  if (id > tours.length) {
-    return res
-      .status(404)
-      .json({ status: 'failed', data: { message: 'tour not found' } });
-  }
   res.status(200).json({
     status: 'success',
     data: {
--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 
-const {createTour, getAllTours, getTour,updateTour, deleteTour, checkID, checkBody} = tourController;
+const { createTour, getAllTours, getTour, updateTour, deleteTour } =
+  tourController;
 
 const router = express.Router();
 // a param middleware that activates only on the tour sub application
-router.param('id', checkID)
-router.route('/').get(getAllTours).post(checkBody,createTour);
+// router.param('id', tourController.checkID);
+router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
