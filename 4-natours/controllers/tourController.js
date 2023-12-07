d8106889fbe37560518fbe5596c3b2d8323a2d63
Omoyajowo Temidayo <temideewan@gmail.com>
2023-12-07 18:34:41 +0100

    feat: aggregate pipeline for business features

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -126,16 +126,99 @@ exports.getTourStats = async (req, res) => {
           avgPrice: -1,
         },
       },
-      {
-        $match: {
-          _id: { $ne: 'easy' },
-        },
-      },
+      // {
+      //   $match: {
+      //     _id: { $ne: 'easy' },
+      //   },
+      // },
     ]);
     res.status(200).json({
       status: 'success',
       data: {
         stats,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      // function not allowed in my atlas tier 🥹
+      // {
+      //   $addFields: {
+      //     month: {
+      //       $function: {
+      //         body: function (month) {
+      //           const months = [
+      //             'January',
+      //             'February',
+      //             'March',
+      //             'April',
+      //             'May',
+      //             'June',
+      //             'July',
+      //             'August',
+      //             'September',
+      //             'October',
+      //             'November',
+      //             'December',
+      //           ];
+      //           return months[month - 1];
+      //         },
+      //         args: ['$_id'],
+      //         lang: 'js',
+      //       },
+      //     },
+      //   },
+      // },
+      {
+        $addFields: {
+          month: '$_id',
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: {
+          numTourStarts: -1,
+        },
+      },
+      {
+        $limit: 6,
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
       },
     });
   } catch (err) {
--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -9,6 +9,7 @@ const {
   deleteTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = tourController;
 
 const router = express.Router();
@@ -16,6 +17,7 @@ const router = express.Router();
 // router.param('id', tourController.checkID);
 router.route('/top-5-tours').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
