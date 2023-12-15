d06bf37cf7a150c21977b0f2ce7b321efc8a417a
Omoyajowo Temidayo <temideewan@gmail.com>
2023-12-15 09:31:27 +0100

    feat: introduce catch all for unrecognised routes

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -12,11 +12,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  // console.log('Hello from the middleware 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -24,6 +19,12 @@ app.use((req, res, next) => {
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
 
