ced0d2e5e8b035868fa6e315dfd2e96c3b898b9a
Omoyajowo Temidayo <temideewan@gmail.com>
2023-12-20 14:50:18 +0100

    feat: send error depending on environments

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,8 +1,32 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const sendDevError = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    stack: err.stack,
+    error: err,
   });
+};
+
+const sendProdError = (err, res) => {
+  // trusted and expected operational error
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  if (process.env.NODE_ENV === 'development') {
+    sendDevError(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendProdError(err, res);
+  }
 };
