30cc0ec92c69c221c63ebf247d9c5a2008730328
Omoyajowo Temidayo <temideewan@gmail.com>
2023-12-22 20:58:19 +0100

    feat: create bare minimum endpoint to sign up users:

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const { getAllUsers, createUser, getUser, updateUser, deleteUser } =
   userController;
-
+const { signup } = authController;
 const router = express.Router();
+router.post('/signup', signup);
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
