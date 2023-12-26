cde6ea8db259eb88bf744fdf6aab2e2341e0d1da
Omoyajowo Temidayo <temideewan@gmail.com>
2023-12-26 20:47:28 +0100

    feat: begin implementation of route protection using jwt

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -48,3 +48,27 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) get the token and check if it exists
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access', 401),
+    );
+  }
+  // 2) Verification token
+
+  // 3) check if user still exists
+
+  // 4) Check if user changed password after the token was issued
+
+  next();
+});
--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authController');
 
+const { protect } = authController;
 const {
   createTour,
   getAllTours,
@@ -18,7 +20,7 @@ const router = express.Router();
 router.route('/top-5-tours').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(protect, getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
