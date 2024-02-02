37c9f7e310e7aea93fa477c6db0705fdd71fafa9
Omoyajowo Temidayo <temideewan@gmail.com>
2024-02-02 05:07:23 +0100

    feat: introduce pug for templating

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,6 +14,12 @@ const reviewRouter = require('./routes/reviewRoutes');
 const { whiteListedParams } = require('./constants/appConstants');
 
 const app = express();
+
+// server side rendering
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+// serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Global MIDDLEWARES
 // Set security HTTP headers
@@ -49,8 +56,6 @@ app.use(
     whitelist: [...whiteListedParams],
   }),
 );
-// serving static files
-app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -58,6 +63,12 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'John',
+  });
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
