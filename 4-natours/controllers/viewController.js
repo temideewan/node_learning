21b27048d7d1ed700497641d23bf4dae58e33c6d
Omoyajowo Temidayo <temideewan@gmail.com>
2024-02-05 13:53:50 +0100

    feat: create a tour overview page and serve it with server rendered content

--- 4-natours/controllers/viewController.js
+++ 4-natours/controllers/viewController.js
@@ -1,8 +1,19 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+
+const catchAsync = require('../Utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //  get tour data from collection
+  const tours = await Tour.find();
+
+  // build template
+
+  // render template using the tour
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
