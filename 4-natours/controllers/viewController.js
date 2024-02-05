b7d6678c118aa70bdd31180abeabe0cc1ddd189a
Omoyajowo Temidayo <temideewan@gmail.com>
2024-02-05 18:12:31 +0100

    feat: create tour details page

--- 4-natours/controllers/viewController.js
+++ 4-natours/controllers/viewController.js
@@ -20,3 +20,12 @@ exports.getTour = (req, res) => {
     title: 'The Forest Hiker',
   });
 };
+exports.getTourFromSlug = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  res.status(200).render('tour', {
+    tour,
+  });
+});
--- 4-natours/routes/viewRoutes.js
+++ 4-natours/routes/viewRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
-const { getOverview, getTour } = require('../controllers/viewController');
+const {
+  getOverview,
+  getTour,
+  getTourFromSlug,
+} = require('../controllers/viewController');
 
 const router = express.Router();
 router.get('/', getOverview);
 router.get('/tour', getTour);
+router.get('/tour/:slug', getTourFromSlug);
 
 module.exports = router;
